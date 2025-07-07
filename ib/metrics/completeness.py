0ba6533152173c2d6a4c4da00b2cdc74756ba1ef
Oleg Magnes <oleg.magnes@gmail.com>
2025-07-07 22:35:24 +0200

    fastercompletness and no artifact metric

--- ib/metrics/completeness.py
+++ ib/metrics/completeness.py
@@ -20,7 +20,6 @@ class Completeness:
     ):
         self.vertices = vertices
         self.labels = labels
-        self.tree = KDTree(self.vertices)
 
     @classmethod
     def from_pointcloud(
@@ -42,39 +41,20 @@ class Completeness:
         )
         return cls(vertices, labels)
 
-    def _compute_completeness(
-        self,
-        source_vertices: np.ndarray,
-        source_tree: KDTree,
-        target_tree,
-        radius: float,
-    ) -> np.ndarray:
-        neighbor_indices: list[list] = target_tree.query_ball_tree(
-            source_tree, r=radius
-        )
-        match = np.zeros(len(source_vertices))
-        for i, indices in enumerate(neighbor_indices):
-            if len(indices) != 0:
-                match[i] = 1.0
-        return match
-
     def __call__(
         self,
         pred_vertices: np.ndarray,
-        radii: list[float] = [0.002, 0.003],  # 0.2, 0.3 % of a scene
+        radii: list[float] = [0.002, 0.003],  # 0.2%, 0.3 % of a scene
     ) -> dict[str, float]:
 
         results = {}
 
         pred_tree = KDTree(pred_vertices)
-
-        # Completeness
+        distances, _ = pred_tree.query(self.vertices, workers=-1)
         for radius in radii:
 
+            matches = (distances <= radius).astype(float)
             str_radius = str(radius).replace(".", "")
-            matches = self._compute_completeness(
-                pred_vertices, pred_tree, self.tree, radius
-            )
             results[f"metrics_main/completeness_{str_radius}"] = float(matches.mean())
 
             label_indices = np.unique(self.labels)
@@ -93,33 +73,6 @@ class Completeness:
             mask_label = self.labels > 0
             results[f"metrics_main/completeness_low_freq_{str_radius}"] = float(
                 matches[mask_label].mean()
-            )
-
-        # Artifacts
-        for radius in radii:
-
-            str_radius = str(radius).replace(".", "")
-            matches = self._compute_completeness(
-                self.vertices, self.tree, pred_tree, radius
-            )
-            results[f"metrics_main/artifacts_{str_radius}"] = float(1 - matches.mean())
-
-            label_indices = np.unique(self.labels)
-            for label_inx in label_indices:
-                label_name = INX_TO_LABEL.get(label_inx, "unknown")
-                mask_label = self.labels == label_inx
-                results[f"metrics_labels/artifacts_{label_name}_{str_radius}"] = float(
-                    1 - matches[mask_label].mean()
-                )
-
-            mask_label = self.labels < 0
-            results[f"metrics_main/artifacts_high_freq_{str_radius}"] = float(
-                1 - matches[mask_label].mean()
-            )
-
-            mask_label = self.labels > 0
-            results[f"metrics_main/artifacts_low_freq_{str_radius}"] = float(
-                1 - matches[mask_label].mean()
             )
 
         return results
